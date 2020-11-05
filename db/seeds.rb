0f0762613894a3782860e9bb87bbf20467952adb
yuanxizhang <yuanxi.zhang@gmail.com>
2020-11-04 23:50:16 -0800

    Update seed.rb with image links

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Provider.destroy_all
+Offer.destroy_all
+Traveler.destroy_all
+Plan.destroy_all
+
 traveler_list = [
     ["Max", "Hiking"],
     ["Jack", "Rock Climbing"],
@@ -12,15 +17,15 @@ traveler_list.each do |name, passion|
 end
 
 plan_list = [
-    ["Alaska", "Sail through the Inside Passage of Alaska on a cruiseship.", 18],
-    ["Iceland", "Visit the Golden Circle, the Blue Lagoon", 13],
-    ["Japan", "Visit imperial palaces in Kyoto, sample sake, take sushi-making lessons.", 13],
-    ["Hawaii", "Visit Waikiki beach in Oahu", 17],
-    ["Costa Rica", "Visit Monteverde Cloud Forest, Tortuguero National Park, and Arenal Volcano", 14],
-    ["Northern Italy", "Visit Venice, Verona, Cortina, and Lake Garda", 17],
-    ["The Danube", "Sail though three countries in Danube river cruise, vist Budapest, Vienna, Salzburg, and Regensburg", 18],
-    ["Ireland", "Visit St. Patricks Cathedral and Trinity College in Dublin, visit King John's Castle and St. Mary's Cathedral in Limerick", 18],
-    ["France", "Visit Chateau de Villandry in the Loire Valley", 17]
+    ["Alaska", "Sail through the Inside Passage of Alaska on a cruiseship.", 6],
+    ["Iceland", "Visit the Golden Circle, the Blue Lagoon", 1],
+    ["Japan", "Visit imperial palaces in Kyoto, sample sake, take sushi-making lessons.", 1],
+    ["Hawaii", "Visit Waikiki beach in Oahu", 5],
+    ["Costa Rica", "Visit Monteverde Cloud Forest, Tortuguero National Park, and Arenal Volcano", 2],
+    ["Northern Italy", "Visit Venice, Verona, Cortina, and Lake Garda", 5],
+    ["The Danube", "Sail though three countries in Danube river cruise, vist Budapest, Vienna, Salzburg, and Regensburg", 6],
+    ["Ireland", "Visit St. Patricks Cathedral and Trinity College in Dublin, visit King John's Castle and St. Mary's Cathedral in Limerick", 6],
+    ["France", "Visit Chateau de Villandry in the Loire Valley", 5]
 ]
 
 plan_list.each do |place, adventure, traveler_id|
@@ -45,6 +50,7 @@ offer_list = [
      "Experience lush green countryside and dramatic coastal cliff",
      "throughout the year",
      "7 days",
+     "https://www.ireland-information.com/public-domain-photographs-of-ireland/photos/thumbnails/Dunguire-Castle-Dunguaire-Galway-s1.jpg",
      1980.00, 
      1],
     ["Romantic Danube", 
@@ -52,33 +58,38 @@ offer_list = [
     	"monthly starting in April",
     	"8 days",
     	1999.00,
+        "https://previews.123rf.com/images/kanuman/kanuman1504/kanuman150400125/38639786-budapest-in-hungary.jpg",
     	2],
     ["The Wonders of Alaska", 
     	"Sail up the west coast of North America into the Inside Passage of Alaska, cruise Glacier Bay National Park, offer onboard cooking workshop",
     	"April to September each year",
     	"21 days",
     	3149.00,
+        "https://previews.123rf.com/images/ramunas/ramunas1701/ramunas170100095/70698856-the-scenic-view-of-a-glacier-in-glacier-bay-national-park-alaska-.jpg",
     	3],
     ["Veneto Villa Escape", 
     	"A beautiful trip to Northern Italy, stay at a countryside villa, enjoy the region's best wines with local cuisine",
     	"April to October each year",
     	"4 days/3 nights",
     	975.00,
+        "https://previews.123rf.com/images/freeartist/freeartist1508/freeartist150800106/44685734-beautiful-lago-di-como-lenno-italy.jpg",
     	4],
     ["Historial France", 
     	"Visit the sites of D-Day landings on Normandy beaches, revell at Chateau d'Usse",
     	"April to October each year",
     	"8 days/7 nights",
     	2850.00,
+        "https://previews.123rf.com/images/macsim/macsim1810/macsim181000043/110084841-top-view-of-the-mont-saint-michel-bay-normandy-france.jpg",
     	5],
     ["Costa Rica's Eco Adventure", 
     	"Take a seven-day journey through emerald rainforests of Costa Ricawith exotic wild life and breathtaking landscape",
     	"year-round",
     	"7 days/6 nights",
     	1725.00,
+        "https://previews.123rf.com/images/kamchatka/kamchatka1801/kamchatka180100091/94040786-majestic-waterfall-in-the-rainforest-jungle-of-costa-rica-tropical-hike-.jpg",
     	6]
 ]
 
-offer_list.each do |tour_name, about, departs, length, price, provider_id|
-  Offer.create( tour_name: tour_name, about: about, departs: departs, length: length, price: price, provider_id: provider_id)
+offer_list.each do |tour_name, about, departs, length, price, image, provider_id|
+  Offer.create( tour_name: tour_name, about: about, departs: departs, length: length, price: price, image: image, provider_id: provider_id)
 end
